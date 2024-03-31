ebee31570fc2d6aa1db39a4658fe713a462ff695
Katrine1996 <Katujha_m_1996@mail.ru>
2024-03-31 14:12:06 +0400

    Changed the file index.html and script.js

---
+++ script.js
@@ -0,0 +1,89 @@
+//* Задание 1
+let a = 20;
+alert(a);
+
+//* Задание 2
+const jahr = 2007;
+alert(jahr);
+
+//* Задание 3
+const name = 'BrendanEich';
+alert(name);
+
+//* Задание 4
+let sum = 10 + 2;
+alert(sum);
+
+let difference = 10 - 2;
+alert(difference);
+
+let product = 10 * 2;
+alert(product);
+
+let quotient = 10 / 2;
+alert(quotient);
+
+//* Задание 5
+let result = 2 ** 5;
+alert(result);
+
+//* Задание 6
+let remainder = 9 % 2;
+alert (remainder);
+
+//* Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert (num);
+
+//* Задание 8
+let age = prompt('Сколько вам лет?');
+alert (age);
+
+//* Задание 9.0
+const user = {
+    name: 'Kett',
+    age: '27',
+    isAdmin: true,
+};
+//* Задание 9.1
+user.cityOfResidence = ('Самара');
+
+//* Задание 9.2
+user.age = 28;
+
+//* Задание 9.3
+delete user.cityOfResidence;
+
+//* Задание 9.4
+let info = prompt("Какую информацию хотите узнать о пользователе?", "name");
+console.log(user[info]);
+
+//* Задание 10
+let moniker = prompt("Как тебя зовут?");
+alert ("Hello, moniker!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
