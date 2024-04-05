0f7e8268f71a6f84ff8c91d66e9f56ad62a75c95
Katrine1996 <Katujha_m_1996@mail.ru>
2024-04-05 14:42:40 +0400

    Added changes script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //* Задание 1
-let a = 20;
+let a = 10;
 alert(a);
+alert (a * 2);
 
 //* Задание 2
 const jahr = 2007;
@@ -11,25 +12,20 @@ const name = 'BrendanEich';
 alert(name);
 
 //* Задание 4
-let sum = 10 + 2;
-alert(sum);
-
-let difference = 10 - 2;
-alert(difference);
-
-let product = 10 * 2;
-alert(product);
-
-let quotient = 10 / 2;
-alert(quotient);
+let x = 10;
+let y = 2;
+alert(x + y);
+alert(x - y);
+alert(x * y);
+alert(x / y);
 
 //* Задание 5
 let result = 2 ** 5;
 alert(result);
 
 //* Задание 6
-let remainder = 9 % 2;
-alert (remainder);
+let num1 = 9, num2 = 2;
+alert (num1 % num2);
 
 //* Задание 7
 let num = 1;
@@ -48,17 +44,18 @@ alert (age);
 //* Задание 9.0
 const user = {
     name: 'Kett',
-    age: '27',
+    age: 27,
     isAdmin: true,
 };
 //* Задание 9.1
-user.cityOfResidence = ('Самара');
+console.log(user);
+user['city of residence'] = 'Самара';
 
 //* Задание 9.2
 user.age = 28;
 
 //* Задание 9.3
-delete user.cityOfResidence;
+delete user['city of residence'];
 
 //* Задание 9.4
 let info = prompt("Какую информацию хотите узнать о пользователе?", "name");
@@ -66,7 +63,8 @@ console.log(user[info]);
 
 //* Задание 10
 let moniker = prompt("Как тебя зовут?");
-alert ("Hello, moniker!");
+alert("Hello, ${moniker}!");
+
 
 
 
